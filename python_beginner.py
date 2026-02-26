4ba3146ed7ac4f91724b8b33b46a560e39479cd3
adamgd91 <agdavidson91@gmail.com>
2026-02-26 10:57:28 -0500

    month and grade

--- python_beginner.py
+++ python_beginner.py
@@ -7,3 +7,24 @@ extra_credit = 10
 
 if score >= 60 or extra_credit >=10:
     print("Pass")
+    
+grade = 85
+
+if grade >=90:
+    print("A")
+elif grade >=80:
+    print("B")
+elif grade >=80:
+    print("C")
+else:
+    print("F")
+    
+month = 7
+if month in [12, 1, 2]:
+    print("Winter")
+elif month in [3, 4, 5]:
+    print("Spring")
+elif month in [6, 7, 8]:
+    print("Summer")
+else:
+    print("Fall")
