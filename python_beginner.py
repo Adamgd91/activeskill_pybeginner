033f21d96b904f7f18287c3b6d80590e7712c1c1
adamgd91 <agdavidson91@gmail.com>
2026-02-26 10:49:08 -0500

    pass

--- python_beginner.py
+++ python_beginner.py
@@ -1,2 +1,9 @@
 original_price = 100
-discounted_price = original / .80
+discounted_price = original_price * .80
+print(discounted_price)
+
+score = 55
+extra_credit = 10
+
+if score >= 60 or extra_credit >=10:
+    print("Pass")
