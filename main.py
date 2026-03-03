8edd123788dbb0ec2f1a7f94f1eb115a1ee6a548
adamgd91 <agdavidson91@gmail.com>
2026-03-03 10:55:05 -0500

    learning split

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 name = "adam"
+
+
+csv = "age, 33, 123 , 11111, adam"
+split_csv = csv.split(",")
+
+testing_csv = []
+for i in split_csv:
+    if i == "age":
+        testing_csv.append(i)
+
+print(testing_csv)
